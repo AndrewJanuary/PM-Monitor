fc57084bb008d368b1fc322cf23da67109667bb7
andrewjanuary <andrewajjanuary@gmail.com>
2021-12-18 23:16:59 +0000

    Add tests for PM prints

--- tests/test_offline.py
+++ tests/test_offline.py
@@ -6,3 +6,15 @@ def test_init():
     offline_mode = Offline('pm25_filename', 'pm10_filename')
     assert offline_mode.pm_two_five_file == 'pm25_filename'
     assert offline_mode.pm_ten_file == 'pm10_filename'
+
+def test_print_pm_two_five(capsys):
+    offline_mode = Offline('pm25_filename', 'pm10_filename')
+    offline_mode.print_pm_two_five(7.77)
+    captured = capsys.readouterr()
+    assert "PM 2.5 7.77\n" in captured.out
+
+def test_print_pm_ten(capsys):
+    offline_mode = Offline('pm25_filename', 'pm10_filename')
+    offline_mode.print_pm_ten(7.77)
+    captured = capsys.readouterr()
+    assert "PM 10 7.77\n" in captured.out
